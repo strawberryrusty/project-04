90abbb8a3ca3fbcbd6c010f2759fcbe9dc86763b
Ejike Chiboka <ejike.chiboka@gmail.com>
2019-09-10 15:44:50 +0100

    item ordering

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -37,21 +37,26 @@ class Navbar extends React.Component {
       <nav className="navbar">
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item">
-
-            </Link>
+            <Link to ="/" className="navbar-item">🏋🏾‍</Link>
 
             <a
               role="button"
               className={`navbar-burger ${this.state.navbarOpen ? 'is-active' : ''}`}
               onClick={this.toggleNavbar}
             >
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
+
+              <span aria-hidden = "true"></span>
+              <span aria-hidden = "true"></span>
+              <span aria-hidden = "true"></span>
             </a>
 
+
+
+
+
+
           </div>
+
 
           <div className={`navbar-menu ${this.state.navbarOpen ? 'is-active' : ''}`}>
             <div className="navbar-start">
@@ -63,6 +68,8 @@ class Navbar extends React.Component {
             </div>
 
             <div className="navbar-end">
+              {!Auth.isAuthenticated() && <Link to="/location" className="navbar-item">🏴󠁧󠁢󠁥󠁮󠁧󠁿 United Kingdom</Link>}
+              {!Auth.isAuthenticated() && <Link to="/help" className="navbar-item">Help</Link>}
               {!Auth.isAuthenticated() && <Link to="/register" className="navbar-item">Register</Link>}
               {!Auth.isAuthenticated() && <Link to="/login" className="navbar-item">Login</Link>}
               {Auth.isAuthenticated() && <a onClick={this.logout} className="navbar-item">Logout</a>}
