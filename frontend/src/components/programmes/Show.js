2a95d23f659e3f61e40c4a8899ffa44188538366
Prash Mohan <prashmohanuon@gmail.com>
2019-09-11 16:46:40 +0100

    centered the programmes index

--- frontend/src/components/programmes/Show.js
+++ frontend/src/components/programmes/Show.js
@@ -95,68 +95,68 @@ class ProgrammesShow extends React.Component {
         <div className="container">
 
           {!this.state.programme && <h2 className="title is-2">Loading...</h2>}
+          <div className="columns is-centered">
+            <div className="column is-half-tablet">
+
+              <div key={this.state.programme.id}>
+
+                <h1 className="title is-2">{this.state.programme.name}</h1>
+                {this.canModify() && <div className="buttons">
+                  <Link
+                    className="button"
+                    to={`/programmes/${this.state.programme.id}/edit`}
+                  >Edit Programme</Link>
+
+                  <button className="button is-danger"
+                    onClick={this.handleDelete}>Delete Programme</button>
+                </div>}
+
+                <div><Link to={`/programmes/${this.state.programme.id}/items/new`} className="button">Add Item</Link></div>
 
 
-          <div className="column is-half-tablet">
+                {this.state.programme.items.map(item =>
 
-            <div key={this.state.programme.id}>
-
-              <h1 className="title is-2">Name:{this.state.programme.name}</h1>
-              {this.canModify() && <div className="buttons">
-                <Link
-                  className="button"
-                  to={`/programmes/${this.state.programme.id}/edit`}
-                >Edit Programme</Link>
-
-                <button className="button is-danger"
-                  onClick={this.handleDelete}>Delete Programme</button>
-              </div>}
-
-              <div><Link to={`/programmes/${this.state.programme.id}/items/new`} className="button">Add Item</Link></div>
-
-
-              {this.state.programme.items.map(item =>
-
-                <div key={item.id} className="card">
-                  <div className="card-header">
-                    <h2 className={`card-header-title title is-4 tag is-${item.day}`}> Day:{item.day}</h2>
-                  </div>
-                  <div className="card-content">
-                    <h2 className="content text"><span className="has-text-weight-semibold">Exercise: </span>{item.exercise.name}</h2>
-                    <h2 className="content text"><span className="has-text-weight-semibold">Description: </span>{item.exercise.description}</h2>
-                    <img className="ShowImage" src={item.exercise.image} alt={item.exercise.name}/>
-                    {item.personalbests.length > 0 &&
-                      <h2 className="content text"><span className="has-text-weight-semibold">Personal Best: </span>{item.personalbests.slice(-1)[0].weight}</h2>
-                    }
-                    <h2 className="content text"><span className="has-text-weight-semibold">Sets: </span>{item.sets}</h2>
-                    <h2 className="content text"><span className="has-text-weight-semibold">Reps: </span>{item.reps}</h2>
-                  </div>
-                  <hr/>
-                  <div className="buttons is-centered">
-                    <div><Link to={`/programmes/${this.state.programme.id}/items/${item.id}/edit`} className="button">Edit Item</Link></div>
-                    <button id={item.id} className="button is-danger"
-                      onClick={this.handleDeleteItems}>Delete Item</button>
-                  </div>
-                  <form id={item.id} onSubmit={this.handleNewPBSubmit}>
-                    <div className="container">
-                      <div className="field">
-                        <label className="label">New PB</label>
-                        <input
-                          className="input"
-                          type="number"
-                          name="weight"
-                          min="0"
-                          placeholder="Input desired new Personal Best(kg)"
-                          onChange={this.handleChange}
-                          value={this.state.formData.weight || ''}
-                        />
-                      </div>
+                  <div key={item.id} className="card">
+                    <div className="card-header">
+                      <h2 className={`card-header-title title is-4 tag is-${item.day}`}> Day:{item.day}</h2>
                     </div>
-                    <button className="button is-centered is-link is-rounded" on>Submit</button>
-                  </form>
-                  <hr/>
-                </div>
-              )}
+                    <div className="card-content">
+                      <h2 className="content text"><span className="has-text-weight-semibold">Exercise: </span>{item.exercise.name}</h2>
+                      <h2 className="content text"><span className="has-text-weight-semibold">Description: </span>{item.exercise.description}</h2>
+                      <img className="ShowImage" src={item.exercise.image} alt={item.exercise.name}/>
+                      {item.personalbests.length > 0 &&
+                        <h2 className="content text"><span className="has-text-weight-semibold">Personal Best: </span>{item.personalbests.slice(-1)[0].weight}</h2>
+                      }
+                      <h2 className="content text"><span className="has-text-weight-semibold">Sets: </span>{item.sets}</h2>
+                      <h2 className="content text"><span className="has-text-weight-semibold">Reps: </span>{item.reps}</h2>
+                    </div>
+                    <hr/>
+                    <div className="buttons is-centered">
+                      <div><Link to={`/programmes/${this.state.programme.id}/items/${item.id}/edit`} className="button">Edit Item</Link></div>
+                      <button id={item.id} className="button is-danger"
+                        onClick={this.handleDeleteItems}>Delete Item</button>
+                    </div>
+                    <form id={item.id} onSubmit={this.handleNewPBSubmit}>
+                      <div className="container">
+                        <div className="field">
+                          <label className="label">New PB</label>
+                          <input
+                            className="input"
+                            type="number"
+                            name="weight"
+                            min="0"
+                            placeholder="Input desired new Personal Best(kg)"
+                            onChange={this.handleChange}
+                            value={this.state.formData.weight || ''}
+                          />
+                        </div>
+                      </div>
+                      <button className="button is-centered is-link is-rounded" on>Submit</button>
+                    </form>
+                    <hr/>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
